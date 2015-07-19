891c8611237a91bb9b77f110ce3a2c0a5c4f203a
Britt Gresham <brittcgresham@gmail.com>
2015-07-19 16:02:10 -0700

    Add noun phrases to collected data

--- model.py
+++ model.py
@@ -5,7 +5,15 @@ import json
 
 import dateutil.parser
 
-from sqlalchemy import (Column, Integer, String, DateTime, ForeignKey, Float)
+from sqlalchemy import (
+    Column,
+    Table,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    Float,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy import create_engine
@@ -13,6 +21,13 @@ from sqlalchemy import create_engine
 from textblob import TextBlob
 
 Base = declarative_base()
+
+noun_phrase_to_message_table = Table(
+    'noun_phrase_to_message_table',
+    Base.metadata,
+    Column('noun_phrase_id', Integer, ForeignKey('noun_phrases.id')),
+    Column('message_id', Integer, ForeignKey('messages.id')),
+)
 
 
 class Contact(Base):
@@ -33,17 +48,22 @@ class Contact(Base):
 
 
 class Message(Base):
-    __tablename__ = 'message'
+    __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True)
     message = Column(String)
     sent = Column(DateTime)
     from_id = Column(Integer, ForeignKey('contacts.id'))
+    to = Column(String)
     polarity = Column(Float)
     subjectivity = Column(Float)
-    nouns = Column(String)
 
     message_to = relationship("Contact", back_populates='messages')
+    nouns = relationship(
+        "NounPhrase",
+        secondary=noun_phrase_to_message_table,
+        backref='messages',
+    )
 
     def __init__(self, message, from_id, to, sent):
         self.message = message
@@ -52,13 +72,27 @@ class Message(Base):
         self.sent = sent
 
     def __repr__(self):
-        return '<Message %s>' % (self.message)
+        return '<Message %s>' % (self.message, )
+
+
+class NounPhrase(Base):
+    __tablename__ = 'noun_phrases'
+
+    id = Column(Integer, primary_key=True)
+    phrase = Column(String)
+
+    def __init__(self, phrase):
+        self.phrase = phrase
+
+    def __repr__(self):
+        return "<Noun Phrase %s>" % (self.phrase, )
 
 
 def import_data():
     session = create_session()
 
     contacts = {}
+    nouns = {}
     messages = []
 
     f = open('output.json', 'r')
@@ -71,6 +105,14 @@ def import_data():
                 session.flush()
                 contacts[message['number']] = contact
             tb = TextBlob(message['content'])
+            final_nouns = []
+            for noun in tb.noun_phrases:
+                if noun not in nouns:
+                    n = NounPhrase(noun)
+                    session.add(n)
+                    session.flush()
+                    nouns[noun] = n
+                final_nouns.append(nouns[noun])
             m = Message(
                 message['content'],
                 contacts[message['number']].id,
@@ -79,11 +121,13 @@ def import_data():
             )
             m.polarity = tb.sentiment.polarity
             m.subjectivity = tb.sentiment.subjectivity
+            m.nouns = final_nouns
             session.add(m)
-            print "%s: Polarity / Subjectivity: (%s / %s)" % (
+            print "%s: Polarity / Subjectivity: (%s / %s), Nouns: %s" % (
                 m,
                 tb.sentiment.polarity,
                 tb.sentiment.subjectivity,
+                tb.noun_phrases,
             )
         session.flush()
         session.commit()
